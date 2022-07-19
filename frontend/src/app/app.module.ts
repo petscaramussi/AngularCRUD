13d2f5343aaf6f98148f807b592f4a08e331ca6d
Peterson <44592787+petscaramussi@users.noreply.github.com>
2022-07-19 17:45:40 -0300

    add Routing on Nav and change Home component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,7 +11,13 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule} from'@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list'
+import { MatCardModule} from'@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 
 
 @NgModule({
@@ -19,7 +25,12 @@ import { MatListModule } from '@angular/material/list'
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +38,8 @@ import { MatListModule } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
